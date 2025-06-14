047d2094b37c7347e02b9940157723e4a1f4a7d7
Alexander Eklöf <alexander.eklof@iths.se>
2025-06-14 21:59:38 +0200

    Removed old project

--- Frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'knock_knock',
-  webDir: 'dist'
-};
-
-export default config;
