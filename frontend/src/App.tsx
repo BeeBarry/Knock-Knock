83724fbca8728df6feffa61714546bd599e691f8
BeeBarry <barry.namdari@gmail.com>
2025-06-14 23:29:15 +0200

    app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
+// src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import TabNav from './components/TabNav';
+import Home from './pages/Home';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
+import Knocks from './pages/Knocks';
+import Chat from './pages/Chat';
 
 function App() {
-  return (
-    <>
-        <h1>Ahhh</h1>
-    </>
-  )
+    return (
+        <Router>
+            <div className="min-h-screen bg-gray-50">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/community" element={<Community />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/knocks" element={<Knocks />} />
+                    <Route path="/chat/:userId" element={<Chat />} />
+                    <Route path="/chat/existing/:chatId" element={<Chat />} />
+                </Routes>
+                <TabNav />
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
