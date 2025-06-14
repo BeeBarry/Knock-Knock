649a44d783efbfe35b6f45d12f4aa4fd9050fdb7
mubuyuk <muratbuyuksal@gmail.com>
2025-06-14 17:19:45 +0200

    added models folder

---
+++ KnockAPI/Models/HelpHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnockAPI.Models
+{
+    public class HelpHistory
+    {
+        public string WithUserName { get; set; } // e.g. "Maria"
+        public string Topic { get; set; }        // e.g. "bicycle recommendations"
+        public DateTime DateUtc { get; set; }    // when it happened
+    }
+}
---
+++ KnockAPI/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace KnockAPI.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }  // “Mr. Zane”
+        public string Title { get; set; }  // “Software Engineer”
+        public string AvatarUrl { get; set; }  // profile image
+        public string Location { get; set; }  // “Gothenburg”
+
+        // embedded arrays:
+        public List<ExpertiseTag> Expertise { get; set; } = new();
+        public List<HelpHistory> PreviousHelps { get; set; } = new();
+    }
+}
